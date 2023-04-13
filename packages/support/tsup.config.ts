c387919b5d45c41ba8a321b76265e54896e416cf
Andrew Goldis <agoldis@gmail.com>
2023-04-13 20:54:44 +0300

    chore: ..

--- packages/support/tsup.config.ts
+++ packages/support/tsup.config.ts
@@ -4,9 +4,8 @@ export default defineConfig({
   sourcemap: true,
   clean: true,
   bundle: true,
+  minify: true,
+  target: 'es2015',
   noExternal: ['rrweb'],
   entry: ['src/index.ts'],
-  loader: {
-    '.src': 'text',
-  },
 });
