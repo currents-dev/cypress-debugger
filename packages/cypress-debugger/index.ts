a3031e10d7e45b99fb9bd93d0899914c0bffec50
Andrew Goldis <agoldis@gmail.com>
2023-07-10 10:15:55 -0700

    fix: reexport typedefs

--- packages/cypress-debugger/index.ts
+++ packages/cypress-debugger/index.ts
@@ -1,36 +1,8 @@
 import { installPlugin } from '@currents/cypress-debugger-plugin';
 import { attachHandlers } from '@currents/cypress-debugger-support';
 
-export type {
-  BrowserLog,
-  ConsoleMessage,
-  LogEntry,
-  LogEntryLevel,
-  LogEntrySource,
-  PluginOptions,
-  RuntimeCallFrame,
-  RuntimeConsoleAPICalled,
-  RuntimeRemoteObject,
-  RuntimeStackTrace,
-  RuntimeStackTraceId,
-  TestExecutionResult,
-} from '@currents/cypress-debugger-plugin';
-export type {
-  BaseEvent,
-  CypressEvent,
-  CypressEventMeta,
-  CypressRawEvent,
-  HeadersEntity,
-  HttpArchiveEntry,
-  HttpArchiveEntryRequest,
-  HttpArchiveEntryResponse,
-  HttpArchiveLog,
-  QueryStringEntity,
-  RRWebEvent,
-  RawEvent,
-  RunContextData,
-  TestState,
-} from '@currents/cypress-debugger-support';
+export type * from '@currents/cypress-debugger-plugin';
+export type * from '@currents/cypress-debugger-support';
 
 export const debuggerSupport = attachHandlers;
 export const debuggerPlugin = installPlugin;
