5e0cbed5cccc4d537ceb14c849326dd08f314a68
Andrew Goldis <agoldis@gmail.com>
2023-08-25 12:22:10 -0700

    chore: fix lint

--- apps/web/src/components/ui/Accordion.tsx
+++ apps/web/src/components/ui/Accordion.tsx
@@ -55,4 +55,4 @@ const AccordionContent = React.forwardRef<
 ));
 AccordionContent.displayName = AccordionPrimitive.Content.displayName;
 
-export { Accordion, AccordionItem, AccordionTrigger, AccordionContent };
+export { Accordion, AccordionContent, AccordionItem, AccordionTrigger };
