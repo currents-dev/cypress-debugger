1ceb8504d26573551d6e0d84dcf80548917b43a5
Eric162 <eric.s.shirley@gmail.com>
2023-09-01 14:29:29 -0400

    trying to change the type reference in the output

--- packages/cypress-debugger/index.ts
+++ packages/cypress-debugger/index.ts
@@ -1,8 +1,8 @@
 import { installPlugin } from '@currents/cypress-debugger-plugin';
 import { attachHandlers } from '@currents/cypress-debugger-support';
 
-export type * from '@currents/cypress-debugger-plugin';
-export type * from '@currents/cypress-debugger-support';
+export type * from '@currents/cypress-debugger-plugin/dist';
+export type * from '@currents/cypress-debugger-support/dist';
 
 export const debuggerSupport = attachHandlers;
 export const debuggerPlugin = installPlugin;
